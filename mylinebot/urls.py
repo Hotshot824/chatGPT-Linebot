51a298f8a31aac48534d7ecd5887339bbf2f49fa
Benson-Hsu <Hotshot824>
2022-12-13 07:56:31 +0000

    feat: add python require.
    1. add environment file.
    
    issue #002

--- mylinebot/urls.py
+++ mylinebot/urls.py
@@ -18,5 +18,5 @@ from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-     path('chatGPTlinebot/', include('chatGPTlinebot.urls')),
+    path('chatGPTlinebot/', include('chatGPTlinebot.urls')),
 ]
