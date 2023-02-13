b9cf67f2aac144fa85c5c05ee80f91c024d55252
Hotshot824 <shen485@gmail.com>
2023-02-13 23:51:35 +0800

    fix: fix django error.
    1. add --noreload to run django command.
    2. add request timeout response.

--- src/chatGPT/API/request.py
+++ src/chatGPT/API/request.py
@@ -11,19 +11,22 @@ class chatGPT():
             return json.load(f)['OPENAI_API']
 
     def Request(self, message):
-        response = requests.post(
-            "https://api.openai.com/v1/completions",
-            headers = {
-                "Authorization": f"Bearer {self.__config['openai_api_key']}",
-                "Content-Type": "application/json",
-            },
-            json={
-                "prompt": message,
-                "model": self.__config['model'],
-                "max_tokens": self.__config['max_token'],
-                "temperature": self.__config['temperature'],
-            },
-            timeout=10,  # Set timeout to 5 seconds
-        )
-        response = response.json()
-        return response['choices'][0]['text'].lstrip()
+        try:
+            response = requests.post(
+                "https://api.openai.com/v1/completions",
+                headers = {
+                    "Authorization": f"Bearer {self.__config['openai_api_key']}",
+                    "Content-Type": "application/json",
+                },
+                json={
+                    "prompt": message,
+                    "model": self.__config['model'],
+                    "max_tokens": self.__config['max_token'],
+                    "temperature": self.__config['temperature'],
+                },
+                timeout=30,  # Set timeout to 10 seconds
+            )
+            response = response.json()
+            return response['choices'][0]['text'].lstrip()
+        except requests.exceptions.RequestException as e:
+            return "Network Error: This request is time out!"
