554352e0447649bedeb6360a40117ddae83864c4
Benson <39863539+Hotshot824@users.noreply.github.com>
2023-06-21 02:51:28 +0800

    add: refactor entrypoint.sh
    issue #benson12

--- init/Linebot/entrypoint.sh
+++ init/Linebot/entrypoint.sh
@@ -1,8 +1,25 @@
 #!/bin/bash
 
 apt-get update > /dev/null && \
-apt-get install jq -y  > /dev/null && \
-curl -s ngrok:4040/api/tunnels | jq -r '.tunnels[0].public_url' | awk '{print "Linebot Listening at " $0 "/chatGPT/callback"}'
+apt-get install jq -y  > /dev/null;
 
-python3 -m pip install -r ./init/requirements.txt --quiet;
+installed=$(pip list --format=freeze | grep -o "^[^=]*");
+
+while IFS= read -r line; do
+  # Get each line package name.
+  package=$(echo "$line" | grep -o "^[^>=<]*");
+
+  # Confirm that the package name exists in the installed.
+  if ! echo "$installed" | grep -q "^$package$"; then
+    echo "Installing requirements...";
+    python3 -m pip install --upgrade pip;
+    python3 -m pip install -r ./init/requirements.txt;
+    break;
+  fi
+done < ./init/requirements.txt
+
+# Get ngrok address then print on screen.
+curl -s ngrok:4040/api/tunnels | jq -r '.tunnels[0].public_url' | awk '{print "Linebot Listening at " $0 "/chatGPT/callback"}';
+
+# Run linebot server
 python3 ./src/manage.py runserver 0.0.0.0:8000 --noreload;
